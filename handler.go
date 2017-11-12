4e8a6051a80f72be81b80b5f50bdfaf78a40dc39
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-11-12 07:34:39 +0530

    Update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package ideago;
 //import "github.com/joomtriggers/ideago/sms"
 
 
-// 
+//  wh
+
 // config = sms.NewConfiguration().SetPassword().SetServer().SetApplication()
 // sms.NewMessage("Message").SendMessage(config);
