a378bb45e5d7de9247ad9fdadc93d5ac6163cd3e
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-12-11 17:54:23 +0530

    Update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- handler.go
+++ handler.go
@@ -1,9 +1,16 @@
 package ideago;
 
-//import "github.com/joomtriggers/ideago/sms"
-
+import "github.com/joomtriggers/ideago/sms"
 
 //  wh
 
 // config = sms.NewConfiguration().SetPassword().SetServer().SetApplication()
 // sms.NewMessage("Message").SendMessage(config);
+
+//proper code to handle a SMS call should be done
+
+func SMS() (*sms.Sender) {
+	sender := &sms.Sender{};
+
+	return sender
+}
