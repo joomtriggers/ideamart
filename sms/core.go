27e4cb6345f9456f780be119368c070fce31a428
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-12-13 13:03:28 +0530

    Update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- sms/core.go
+++ sms/core.go
@@ -8,13 +8,14 @@ import (
 	"encoding/json"
 )
 
-func SendSMSRequest(sender *Sender) SendResponse{
+func SendSMSRequest(sender *Sender) SendResponse {
 	b := new(bytes.Buffer)
 	json.NewEncoder(b).Encode(sender.SendRequest)
-	w, _ := http.NewRequest("POST",sender.Server, b);
+	w, _ := http.NewRequest("POST", sender.Server, b);
 	w.Header.Add("content-type", "application/json");
 	client := &http.Client{};
 	res, _ := client.Do(w);
+	io.Copy(os.Stdout, b)
 	io.Copy(os.Stdout, res.Body)
 	return sender.SendResponse;
 }
